0b71c1ff4cf2435aceec8c3b339d2e63be2681d4
YazanAlrayyes21 <YazanAlrayyes21@21YazanAlrayyes.kingsacademy.edu.jo>
2021-03-28 23:43:01 -0500

    testing this

--- Project/src/tester.java
+++ Project/src/tester.java
@@ -1,5 +1,12 @@
 
 public class tester {
+	private int x=0;
+	
+	public void counter(int z) {
+		while (x<=z) {
+			x++;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
